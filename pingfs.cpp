73d183d091b6e04187c3bc730ee25e9378c5481f
wheelsandmetal <jakob@schmutz.co.uk>
2020-11-14 22:11:14 +0000

    feat: correctly show the data coming back from recv packet

--- pingfs.cpp
+++ pingfs.cpp
@@ -7,9 +7,8 @@
 #include <netinet/ip_icmp.h>
 #include <unistd.h> 
 
-const int MAX_MESSAGE_SIZE = 64;
-
-using namespace std;
+const size_t MAX_MESSAGE_SIZE = 256;
+const size_t IPV4_PING_HEADER_SIZE = 28;
 
 struct data_packet
 {
@@ -49,10 +48,7 @@ int get_sock_fd() {
   int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
   if (sockfd < 0){
     std::cout << "Failed to create Socket" << std::endl;
-    switch (errno) {
-      case 1: std::cout << "Permisssion Denined" << std::endl; break;
-      default: std::cout << hstrerror(errno) << std::endl;
-    }
+    std::cout << hstrerror(errno) << std::endl;
     throw std::exception();
   }
   int ttl = 64;
@@ -70,44 +66,58 @@ int main(int argc, char *argv[]) {
   addr_con.sin_port = htons (0);
   addr_con.sin_addr.s_addr = *(long*)host_entity->h_addr;
   std::string message = argv[2];
+  size_t recv_size = message.size() + IPV4_PING_HEADER_SIZE;
 
   int sockfd;
   try {
     sockfd = get_sock_fd();
-  } catch (std::exception) {
+  } 
+  catch (std::exception)
+  {
     return EXIT_FAILURE;
   }
 
   struct data_packet packet;
-  while (1) {
-    bzero(&packet, sizeof(packet));
-    strcpy(packet.msg, message.c_str());
+  bzero(&packet, sizeof(packet));
+  strcpy(packet.msg, message.c_str());
+  while (1)
+  {
     packet.hdr.type = ICMP_ECHO;
     packet.hdr.checksum = checksum(&packet, sizeof(packet));
-    if ( 0 >= sendto(sockfd, &packet, sizeof(packet), 0,
-          (struct sockaddr*)&addr_con, sizeof(addr_con))
-       )
+    if (0 >= sendto(sockfd, &packet, sizeof(packet), 0, (struct sockaddr*)&addr_con, sizeof(addr_con)))
     {
       std::cout << "Failed to send packet" << std::endl;
-      switch (errno) {
-        case 1: std::cout << "Permisssion Denined" << std::endl; break;
-        default: std::cout << "Check '$man sentto' for errno " << errno << std::endl;
-      }
+      std::cout << hstrerror(errno) << std::endl;
       return EXIT_FAILURE;
     }
+
+    bzero(&packet, sizeof(packet));
     usleep(100000);
 
     struct sockaddr_in r_addr; 
     unsigned int addr_len = sizeof(r_addr);
-    if ( 0 >= recvfrom(sockfd, &packet, sizeof(packet), 0,
-          (struct sockaddr*)&r_addr, &addr_len)
-       )
+    char buffer[recv_size];
+    auto bRead = recvfrom(sockfd, &buffer, sizeof(buffer), 0, (struct sockaddr*)&r_addr, &addr_len);
+    if (bRead == -1)
     {
       std::cout << "Failed to recvpacket errno " << errno << std::endl;
       std::cout << hstrerror(errno) << std::endl;
+      return EXIT_FAILURE;
     }
 
-    std::cout << std::string(packet.msg, MAX_MESSAGE_SIZE) << std::endl;
+    strcpy(packet.msg, std::string(buffer, bRead).substr(IPV4_PING_HEADER_SIZE).c_str());
+    if (bRead == recv_size)
+    {
+      std::cout << "Your data is SECURE in the cloud: \"" << packet.msg << "\"" << std::endl;
+    }
+    else if (bRead < recv_size)
+    {
+      recv_size = bRead;
+      std::cout << "Data was lost by your chosen cloud host.                  Bummer :-(" << std::endl;
+      std::cout << "This is what we recovered:" << std::endl;
+      std::cout << packet.msg << std::endl;
+      return EXIT_FAILURE;
+    }
   }
 
   return EXIT_SUCCESS;
